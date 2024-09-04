382aaf5d071b62cfcd25144c0e4fc25e0746f38c
Hamed <hamed.2012.darzi@gmail.com>
2024-09-04 21:57:11 +0200

    load variables

--- Bubble_Sort.asm
+++ Bubble_Sort.asm
@@ -11,3 +11,33 @@
 #		    }
 #		} 
 ###############################################################################
+
+
+
+
+
+
+
+.globl _start
+.data
+    size:   .word  8                  # size of the array        
+    array:  .dword 8,7,6,5,4,3,2,1    # array of long integers
+
+.text
+_start:
+
+	la t0, array
+	la t1, size
+	lw t1, 0(t1)
+	addi t1, t1, -1		#size-1
+	li t2, 1		#change
+
+
+
+
+
+
+
+
+
+
