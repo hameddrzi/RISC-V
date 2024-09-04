5239225d9a3e9d2ba9957c1254c1f4811f5b995b
Hamed <hamed.2012.darzi@gmail.com>
2024-09-04 21:54:56 +0200

    insert rules

---
+++ Bubble_Sort.asm
@@ -0,0 +1,13 @@
+#		int change = 1;
+#		while (change) {
+#		    change = 0;
+#		    for (int i = 0; i < size-1; i++)  {
+#		        if (array[i] > array[i+1]) {
+#		            long temp = array[i];
+#		            array[i] = array[i+1];
+#		            array[i+1] = temp;
+#		            chance = 1;
+#		        }
+#		    }
+#		} 
+###############################################################################
