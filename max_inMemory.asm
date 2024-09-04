830b5873ee9dfdcfb4f2250b8b05647a72532b6e
Hamed <hamed.2012.darzi@gmail.com>
2024-09-04 19:01:48 +0200

    completed_1

--- max_inMemory.asm
+++ max_inMemory.asm
@@ -9,20 +9,21 @@
 .text
 _start:
 	la t0, array
-	lw t1, 0(t0)
-	la t2, size
-	lw t2, 0(t2)
+	la t1, size
+	lw t2, 0(t0)
+	lw t1, 0(t1)
 	
 loop:
 	lw t3, 0(t0)
-	bge t1, t3, skip1
-	add t1, t3, zero
-skip1:	
+	bge t2, t3, skip1
+	add t2, t3, zero
+skip1: 
 	addi t0, t0, 4
-	addi t2, t2, -1
-	bne t2, zero, loop
+	addi t1, t1, -1
+	bne t1, zero, loop
 	
 	la t0, result
-	sw t1, 0(t0)
+	sw t3, 0(t0)
+	
 	
 	
