9d410a53b03231161b55647299ec7acef72d4ad4
Hamed <hamed.2012.darzi@gmail.com>
2024-09-04 19:41:44 +0200

    first

---
+++ Count_odd_items.asm
@@ -0,0 +1,6 @@
+#Given a word in memory (size), followed by "size" bytes (an array of bytes),
+#write a RISC-V program that counts the total number of odd numbers among the
+#elements in the array. The resulting value should be saved in a variable labeled result.
+
+
+
