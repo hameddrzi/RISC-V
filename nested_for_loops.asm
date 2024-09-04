9d89e512575d78fad8dd73e312c2d79ef1f99318
Hamed <hamed.2012.darzi@gmail.com>
2024-09-04 16:28:48 +0200

    completed

--- nested_for_loops.asm
+++ nested_for_loops.asm
@@ -14,3 +14,20 @@ start:
         li   t2, 0            # R
 
         li   t3, 0           # i=0
+for_loop1:
+	bge t3, zero, end1
+for_loop2:
+	li t4, 0		# j = 0
+	bge t4, t1, end
+	mul t5,t2, 2
+	add t5, t5, t3
+	add t5, t5, t4
+	
+	addi t4, t4, 1
+	j for_loop2
+end:
+	addi t3, t3, 1
+	j for_loop1
+end1:
+	ecall
+	
