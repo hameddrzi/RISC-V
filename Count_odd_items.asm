fc7d21c37772316efcac8582012d15224c79a185
Hamed <hamed.2012.darzi@gmail.com>
2024-09-04 20:25:48 +0200

    load variable

--- Count_odd_items.asm
+++ Count_odd_items.asm
@@ -3,4 +3,17 @@
 #elements in the array. The resulting value should be saved in a variable labeled result.
 
 
+.globl _start
+.data
+    size:   .word 8                  # size of the array        
+    array:  .byte 2,1,3,2,3,-6,1,23  # the array of bytes
+    result: .word 0                  # store the result
+    
+.text
+_start:
 
+	la t0, array
+	la t1, size
+	lw t1, 0(t1)
+	li t3, 1
+	
