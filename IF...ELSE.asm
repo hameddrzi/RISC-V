585f1e25782b61c150de0b22ac75b2d37eb01b00
Hamed <hamed.2012.darzi@gmail.com>
2024-09-04 14:24:54 +0200

    completed
    completed firt algorithm of code

--- IF...ELSE.asm
+++ IF...ELSE.asm
@@ -2,7 +2,19 @@
 .text
 
 start:
-	li t0, 2
+
+  	######################
+        # First code fragment
+        # x = x - y
+        # if (x < 0)
+        #     x = 0
+        # y = y - 1
+        ######################
+	li t0, 2   
 	li t1, 3
 	
-	ffff
+	sub t0, t0, t1
+	bge t0, zero, jump
+	addi t0, zero, 0
+jump:
+	addi t2, t2, -1
