ad19bbb2a0a51f12ffeb98e900d1a2a787a50e90
Hamed <hamed.2012.darzi@gmail.com>
2024-09-04 14:28:38 +0200

    completed

--- IF...ELSE.asm
+++ IF...ELSE.asm
@@ -10,6 +10,8 @@ start:
         #     x = 0
         # y = y - 1
         ######################
+        
+        
 	li t0, 2   
 	li t1, 3
 	
@@ -18,3 +20,30 @@ start:
 	addi t0, zero, 0
 jump:
 	addi t2, t2, -1
+	
+	
+	
+        ######################
+        # Second code fragment
+        # x = (x - 2) + y
+        # if (x < y)
+        #   x = x + 1
+        # else
+        #   y = y + 1
+        ######################
+        
+        
+        addi t0, t0, -2
+        add t0, t0, t1
+        bge t0, t1, else
+        addi t0, t0, 1
+else:
+	addi t1, t1, 1
+        
+        
+        
+        
+        
+        
+        
+        
