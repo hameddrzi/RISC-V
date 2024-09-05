b0bc653b8203dca1a34b2017db7f5ffcff548ceb
Hamed <hamed.2012.darzi@gmail.com>
2024-09-05 16:29:35 +0200

    import variabls

---
+++ ASCII.asm
@@ -0,0 +1,19 @@
+#Write a RISC-V function digit that checks if a byte passed as
+#a parameter in register a0 represents a digit character (0-9)
+#in ASCII encoding. Checking means: return 1 if the condition is true, 0 otherwise.
+
+#####################################################################################
+
+.globl _start
+.data
+    d: .byte '1'
+.text
+_start:
+    # call digit
+    la   a0, d
+    lbu  a0, 0(a0)
+    jal  ra, digit
+
+    #exit
+    li   a7, 10
+    ecall
