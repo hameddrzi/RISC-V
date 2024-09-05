6769a8a907519587abb42f92b6c2052a8ee787d1
Hamed <hamed.2012.darzi@gmail.com>
2024-09-05 16:47:58 +0200

    completed

--- hash_function.asm
+++ hash_function.asm
@@ -28,4 +28,29 @@ _start:
 ##### function part #####
 
 hash:
+	li t0, 5381		#hash
+	li t1, 0		#i
+while_loop:
+	lbu t2, 0(a0)
+	beqz t2, end1
+	
+	slli t3, t0, 5		# shift left 2^5
+	add t0, t0, t3
+	add t0, t0, t2
+	addi t2, t2, 1		# i++
+	lbu t0, 0(a0)
+	j while_loop
+end1:
+	mv a0, t0
+	ret
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
