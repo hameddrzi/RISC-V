8a72e7712eb375cac97626067c6e35a98ec4e692
Hamed <hamed.2012.darzi@gmail.com>
2024-09-05 15:57:33 +0200

    NOT Completed

--- Bubble_Sort.asm
+++ Bubble_Sort.asm
@@ -29,11 +29,17 @@ _start:
 	la t0, array
 	la t1, size
 	lw t1, 0(t1)
-	addi t1, t1, -1		#size-1
-	li t2, 1		#change
-
-
-
+	addi t1, t1, -1
+	li t2, 1
+while:
+	ble t1, zero, end1		# size-1 < 0
+	
+for_loop:
+	ld t3, 0(t0)			#array{i}
+	ld t4, 0(t0)
+	addi t4, t4, 8			#array{i+1}
+	ble t3, t4, end2
+	sd t3, 0(
 
 
 
