db0e606a4965cd6bc8065c51ed95241b0b4e78b9
Hamed <hamed.2012.darzi@gmail.com>
2024-09-05 18:42:31 +0200

    import

---
+++ contains_function.asm
@@ -0,0 +1,38 @@
+#Using RISC-V assembly language, write a contains function that receives:
+
+#In register a0, the memory address of a string (array of bytes)
+#In register a1, a byte representing an ASCII character (low, lower bound)
+#In register a2, a byte representing an ASCII character (high, upper bound)
+#The function returns 1 if the given string contains at least one character within the range [low, high]. The function returns 0 otherwise.
+
+#		int contains(char str[], char low, char high) {
+#		    int i=0;
+#		    while (str[i] != 0x00) {
+#		        if (str[i] >= low && str[i] <= high) {
+#		            return 1;
+#		        }
+#		        i++;
+#		    }
+#		    return 0;
+#		}
+#################################################################################################
+
+.globl _start
+.section .data
+    buffer: .string  "test string"
+    low:    .byte  'A'
+    high:   .byte  'Z'
+
+.text
+_start:
+    # call contains
+    la   a0, buffer
+    la   a1, low
+    la   a2, high
+    lbu  a1, 0(a1)
+    lbu  a2, 0(a2)
+    jal  ra, contains
+
+    # exit
+    li   a7, 10
+    ecall
