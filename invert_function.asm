5d1ad44ba0f56d35d644451a24993b22b9b0f755
Hamed <hamed.2012.darzi@gmail.com>
2024-09-05 21:15:18 +0200

    completed

--- invert_function.asm
+++ invert_function.asm
@@ -30,7 +30,35 @@ _start:
     ecall
 
 ##### function part #####    
-        
-            
+                    
 invert:                    
-                                  
+	addi sp, sp, -32
+	sd ra, 0(sp)
+	sd a0, 8(sp)
+	sd s1, 16(sp)
+	sd s2, 24(sp)
+	
+	addi s1, zero, 0
+	addi s2, a1, -1
+loop_invert:
+	blt s2, s1, end_invert
+	
+	 ld a0, 8(sp)
+	 add a1, s1, zero
+	 add a2, s2, zero
+	 jal ra, swap
+	 addi s1, s1, 1
+	 addi s2, s2, -1
+	 j loop_invert
+end_invert:
+	ld ra, 0(sp)
+	ld s1, 16(sp)
+	ld s2, 24(sp)
+	addi sp, sp, 32
+	ret 
+	                                     
+	                                                                         
+	                                                                                                             
+	                                                                                                                                                 
+	                                                                                                                                                                                     
+	                                                                                                                                                                                                                                                             
