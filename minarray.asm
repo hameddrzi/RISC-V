8603ed7ec13447cc1ba3923b7931a33268954886
Hamed <hamed.2012.darzi@gmail.com>
2024-09-06 12:32:22 +0200

    first

---
+++ minarray.asm
@@ -0,0 +1,45 @@
+#Write a function minarray(array, size) that returns the index of
+#the minimum value in the array (assume the array is not empty, i.e., size > 0).
+
+##################################################################################
+
+#		int minarray(int[] array, int size) {
+#		    int min_idx = 0;
+#		    int min_val = array[0];
+#		    int i = 1;
+
+#		    while (i < size) {
+#		        if (array[i] < min_val) {
+ #           min_idx = i;
+#		            min_val = array[i];
+#		        }
+#		        ++i;
+#		    }
+#		    return min_idx;
+#		}
+
+
+#################################################################################
+
+
+  .globl _start
+  .data
+    size:  .word  10
+    array: .word  3,1,6,6,23,17,9,25,3,4
+  
+  .text
+  _start:
+      # chiama minarray
+      la   a0, array
+      la   a1, size
+      lw   a1, 0(a1)
+      jal  ra, minarray
+      
+      #exit
+      li   a7, 10
+      ecall   
+
+##### function part #####
+minarray:
+	
+
