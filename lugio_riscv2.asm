1cef1367fea6ab724abb1a896200820bcafed349
Hamed <hamed.2012.darzi@gmail.com>
2024-12-09 22:02:43 +0100

    import

---
+++ lugio_riscv2.asm
@@ -0,0 +1,7 @@
+##############################################################################
+		#// int (in C) è equivalente a word (in RISC-V)
+		#// short (in C) è equivalente a half (in RISC-V)
+		#int ones_arr(short arr1[], short arr2[], int pos) {
+		#    return countones(arr1[pos]) * countones(arr2[pos]);
+		#}
+##############################################################################
