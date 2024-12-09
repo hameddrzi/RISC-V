a666f24da7d85d299d779e3cb951c491ea6dd831
Hamed <hamed.2012.darzi@gmail.com>
2024-12-09 22:03:11 +0100

    import

--- lugio_riscv2.asm
+++ lugio_riscv2.asm
@@ -5,3 +5,40 @@
 		#    return countones(arr1[pos]) * countones(arr2[pos]);
 		#}
 ##############################################################################
+
+.globl _start
+.data
+    arr1: .half -129, 127, 128, 127, -128, 127, 128, 127
+    arr2: .half 127, 128, 127, -128, 127, 128, 127, -129
+    pos:  .word 7
+    
+.text
+_start:
+    # chiama ones_arr
+    la   a0, arr1
+    la   a1, arr2
+    la   a2, pos 
+    lw   a2, 0(a2)
+    jal  ra, ones_arr
+
+    # exit
+    li   a7, 10
+    ecall
+
+#******************************************************
+# Procedure countones(n)
+# a0 -> N
+# a0 -> ritorna il conteggio dei bit impostati a 1
+#******************************************************
+countones:
+        li a1, 0
+    ca: beqz a0, cb
+        andi t0, a0, 1
+        add  a1, a1, t0
+        srli a0, a0, 1
+        j ca
+    cb: mv a0, a1
+        ret
+
+#******************************************************
+# completa la funzione ones_arr nel campo sottostante
