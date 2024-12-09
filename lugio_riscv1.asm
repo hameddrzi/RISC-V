ef6406ace41fd82142b57aebf10b6c44e1a9c958
Hamed <hamed.2012.darzi@gmail.com>
2024-12-09 22:00:46 +0100

    completed

--- lugio_riscv1.asm
+++ lugio_riscv1.asm
@@ -27,4 +27,29 @@
     #******************************************************
     # completa la funzione caesar_encrypt nel campo sottostante
 caesar_encrypt:
+	li t0, 0		#i = 0
 	
+for_loop:
+	add t1, t0, a0
+	lbu t2, 0(t1)
+	beq t2, zero, end1
+	
+	li t3, 'a'
+	blt t2, t3, next1
+	li t3, 'z'
+	bgt t2, t3, next1
+	
+	li t3, 'a'
+	li t4, 26
+	sub t2, t2, t3
+	addi t2, t2, 23
+	rem t2, t2, t4
+	add t2, t2, t3
+	
+next1:
+	sb t2, 0(t1)
+	addi t0, t0, 1
+	j for_loop
+end1:
+	#mv a0, t1
+	ret
