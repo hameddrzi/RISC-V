99c90ecfb3e73384125800555000cc044bc2d954
Hamed <hamed.2012.darzi@gmail.com>
2024-12-09 19:30:04 +0100

    import first line

--- riscv2.asm
+++ riscv2.asm
@@ -5,5 +5,31 @@
  		#}
 ##################################################################
 
-				
+   .globl _start
+    .data
+        array: .word 8,5,3,7,2,6,4,1
+        size:  .word 8
+        
+    .text
+    _start:
+        # chiama sumf
+        la   a0, array
+        la   a1, size
+        lw   a1, 0(a1)
+        jal  ra, sumf
+        
+        # uscita
+        li   a7, 10
+        ecall
+    #******************************************************
+    # funzione foo
+    #******************************************************
+    foo:
+        li    t0, 5
+        div   a0, a0, t0
+        slli  a0, a0, 1
+        ret
+    #******************************************************
+    # completare la funzione sumf nel campo di sotto
+    				
  		
